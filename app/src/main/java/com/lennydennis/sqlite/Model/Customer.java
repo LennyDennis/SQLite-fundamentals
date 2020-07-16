c1f6861d208a2dd0b3bbf7d5962d5d88790d2d22
LennyDennis <lennydennis@gmail.com>
2020-07-16 14:45:07 +0300

    Add a cutomer model class

---
+++ app/src/main/java/com/lennydennis/sqlite/Model/Customer.java
@@ -0,0 +1,60 @@
+package com.lennydennis.sqlite.Model;
+
+public class Customer {
+    private int id;
+    private String customerName;
+    private String customerAge;
+    private Boolean isActive;
+
+    public Customer() {
+    }
+
+    public Customer(int id, String customerName, String customerAge, Boolean isActive) {
+        this.id = id;
+        this.customerName = customerName;
+        this.customerAge = customerAge;
+        this.isActive = isActive;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", customerName='" + customerName + '\'' +
+                ", customerAge='" + customerAge + '\'' +
+                ", isActive=" + isActive +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAge() {
+        return customerAge;
+    }
+
+    public void setCustomerAge(String customerAge) {
+        this.customerAge = customerAge;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+}
